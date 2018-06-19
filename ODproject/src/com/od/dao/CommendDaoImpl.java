31d354c1275ac166ebf3e26269a7adfb6eeeabaa
李晓晗 <514504871@qq.com>
2018-06-19 17:14:38 +0800

    基于用户协同过滤

--- ODproject/src/com/od/dao/CommendDaoImpl.java
+++ ODproject/src/com/od/dao/CommendDaoImpl.java
@@ -7,6 +7,9 @@ import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+import com.od.entity.Classes;
+import com.od.entity.Course;
+import com.od.entity.ODMethod;
 import com.od.entity.Score;
 import com.od.entity.User;
 
@@ -14,15 +17,29 @@ import com.od.entity.User;
 public class CommendDaoImpl {
 	@Resource
 	private SessionFactory sessionFactory;
-	
+	//5分评价
 	public List<Score> findScoreByUserId(User u) {
 		Query query = this.sessionFactory.getCurrentSession().createQuery("select * from score where userid ="+u.getId()+"and score = 5");
 		return query.list();
 	}
-
-	public List<User> findUserByUserId() {
-		Query query = this.sessionFactory.getCurrentSession().createQuery("select * from user");
+	//除自己外全部用户
+	public List<User> findUserByUserId(User u) {
+		Query query = this.sessionFactory.getCurrentSession().createQuery("select * from user where id !="+u);
 		return query.list();
+	}
+	//用户同时评价5分的课程数量
+	public int findCountByCourseIdAndUserId(int courseid, int userid) {
+		Query query = this.sessionFactory.getCurrentSession().createQuery("select count(*) from score where courseid="+courseid+"and userid="+userid+"and score = 5");
+		
+		return (int)query.uniqueResult();
+	}
+	public Score findScoreByCourseIdAndUserId(Course course, User u) {
+		Query query = this.sessionFactory.getCurrentSession().createQuery("select * from score where courseid="+course.getId()+"and userid="+u.getId());
+		return (Score)query.uniqueResult();
+	}
+	public Classes findCourseByCourseIdAndODMethodId(ODMethod odMethod, Course course) {
+		Query query = this.sessionFactory.getCurrentSession().createQuery("select * from classes where odmethodId="+odMethod.getId()+"and courseId="+course.getId());
+		return (Classes)query.uniqueResult();
 	}
 
 }
--- ODproject/src/com/od/service/CommendServiceImpl.java
+++ ODproject/src/com/od/service/CommendServiceImpl.java
@@ -1,13 +1,17 @@
 package com.od.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.od.dao.CommendDaoImpl;
 import com.od.entity.Course;
 import com.od.entity.Score;
@@ -19,15 +23,57 @@ public class CommendServiceImpl {
 	@Resource
 	private CommendDaoImpl commendDaoImpl;
 	
-	public Course getCommend(User u) {
+	public Set<Course> getCommend(User u) {
+		//登录用户5分评价
 		List<Score> scores = commendDaoImpl.findScoreByUserId(u);
-		List<User> users = commendDaoImpl.findUserByUserId();
+		//全体用户
+		List<User> users = commendDaoImpl.findUserByUserId(u);
+		//相似度列表
+		Map<User, Double> similarityMap = new HashMap<>();
+		//最大相似度
+		Double maxSimilarity = 0.00;
 		for(User user:users){
+			//相似度
 			Double similarity;
-			int count;
-			//for (Score)
+			//公式分子
+			int numerator = 0;
+			for(Score score:scores){
+				int count = commendDaoImpl.findCountByCourseIdAndUserId(score.getCourse().getId(),user.getId());
+				numerator += count;
+			}
+			//5分评价
+			List <Score> scores2 = commendDaoImpl.findScoreByUserId(user);
+			//公式分母
+			Double denominator = Math.sqrt(scores.size()*scores2.size());
+			//相似度
+			if(denominator == 0){
+				similarity = 0.00;
+			}else {
+				similarity = numerator/denominator;
+			}
+			similarityMap.put(user, similarity);
+			if(similarity > maxSimilarity){
+				maxSimilarity = similarity;
+			}
 		}
+		//最大相似用户
+		List<User> maxSimilarUsers = new ArrayList<User>();
 		
-		return null;
+		for(Entry<User, Double> entry : similarityMap.entrySet()){
+			if(maxSimilarity.equals(entry.getValue())){
+				maxSimilarUsers.add(entry.getKey());
+			}
+		}
+		//基于用户的协同过滤推荐课程
+		Set<Course> courses = new HashSet<Course>();
+		for(User maxSimilarUser:maxSimilarUsers){
+			for(Score c:maxSimilarUser.getScore()){
+				if(c.getScore()==5&&this.commendDaoImpl.findScoreByCourseIdAndUserId(c.getCourse(), u)==null
+						&&this.commendDaoImpl.findCourseByCourseIdAndODMethodId(u.getOdMethod(),c.getCourse())==null){
+					courses.add(c.getCourse());
+				}
+			}
+		}
+		return courses;
 	}
 }
